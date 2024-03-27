bdeb07a415a4139af0ffa0952fe269e583b2836e
DanielSiilva <daniellsousa0614@gmail.com>
2024-03-27 17:45:10 -0300

    fix: add knexfile

---
+++ knexfile.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+module.exports = {
+  client: "mssql",
+  connection: {
+    server: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: parseInt(process.env.DB_PORT),
+    options: {
+      encrypt: true,
+      enableArithAbort: true,
+      trustServerCertificate: true,
+    },
+  },
+};
