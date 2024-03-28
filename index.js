a199f3fa7040c5d44e4a735144e1ae146f3bb5b6
DanielSiilva <daniellsousa0614@gmail.com>
2024-03-28 10:06:34 -0300

    fix: execucao de filtro direto

--- index.js
+++ index.js
@@ -9,12 +9,6 @@ functions.http("helloHttp", async (req, res) => {
   const authHeader = req.headers.authorization;
   const sql = req.body.name;
 
-  let id = "ID" || "id" || "Id";
-  const orderByColumn = req.body.orderByColumn || id;
-  const page = parseInt(req.body.page || req.query.page || 1, 10);
-
-  let pageSize = parseInt(req.body.pageSize || req.query.pageSize || 100, 10);
-
   if (!authHeader) {
     return res
       .status(401)
@@ -37,39 +31,12 @@ functions.http("helloHttp", async (req, res) => {
     });
   }
 
-  const tableNameMatch = sql.match(/FROM dbo\.(\w+)/i);
-  const tableExists = await knex.schema.hasTable(tableNameMatch[1]);
-  if (!tableExists) {
-    return res.status(404).json({ message: "Table not found." });
-  }
-
-  const offset = (page - 1) * pageSize;
-
   try {
-    const countQuery = `SELECT COUNT(*) AS totalCount FROM (${sql}) AS count_table`;
-    const countResult = await knex.raw(countQuery);
-
-    if (countResult[0].totalCount < 100) {
-      pageSize = countResult[0].totalCount;
-    }
-
-    const totalCount = parseInt(countResult[0].totalCount, 10);
-
-    const paginatedSql = `${sql} ORDER BY ${orderByColumn} OFFSET ${offset} ROWS FETCH NEXT ${pageSize} ROWS ONLY`;
-    const paginatedResult = await knex.raw(paginatedSql);
-
-    const data = paginatedResult;
-
-    const totalPages = Math.ceil(totalCount / pageSize);
+    const dataResult = await knex.raw(sql);
+    const data = dataResult;
 
     res.json({
-      data: data,
-      pageInfo: {
-        page,
-        pageSize,
-        totalPages,
-        totalCount,
-      },
+      data,
     });
   } catch (error) {
     console.error("Error executing SQL query:", error);
